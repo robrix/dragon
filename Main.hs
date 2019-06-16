aaaf4177d7d43e9008da294a5d588cec5f6c4ade
Rob Rix <robrix@github.com>
2019-06-16 09:54:41 -0400

    Add some type signatures.

--- Main.hs
+++ Main.hs
@@ -17,6 +17,7 @@ data Segment
   | L
   deriving (Eq, Ord, Show)
 
+rotateCCW :: Segment -> Segment
 rotateCCW U = R
 rotateCCW R = D
 rotateCCW D = L
@@ -27,19 +28,25 @@ data T a
   | T a :-: T a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
+reverse' :: T Segment -> T Segment
 reverse' = foldl1 (flip (:-:)) . fmap Leaf
 
 
+next :: T Segment -> T Segment
 next c = c :-: reverse' (fmap rotateCCW c)
 
+nth :: Int -> T Segment
 nth n = iterate next (Leaf L) !! n
 
 
+renderS :: Segment -> S.Path
 renderS U = S.vr 50
 renderS R = S.hr 50
 renderS D = S.vr (-50)
 renderS L = S.hr (-50)
 
+render :: T Segment -> S.AttributeValue
 render = S.mkPath . (S.m 500 500 >>) . foldl1 (>>) . fmap renderS
 
+doc :: T Segment -> S.Svg
 doc = (S.docTypeSvg ! A.version "1.1") . (S.path ! A.stroke "black" ! A.strokeWidth "1" ! A.fill "none" !) . A.d . render
